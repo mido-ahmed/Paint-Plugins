1ca773411121733730d43e31e02f4ee70ce03f44
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-02-08 15:18:57 +0200

    add plugins to project

---
+++ lib/Paint_plugins/image-painter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:image_painter/image_painter.dart';
+
+class Image_Painter extends StatefulWidget {
+  const Image_Painter({Key? key}) : super(key: key);
+
+  @override
+  State<Image_Painter> createState() => _Image_PainterState();
+}
+
+class _Image_PainterState extends State<Image_Painter> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
