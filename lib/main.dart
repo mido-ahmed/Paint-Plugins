0b2e9e29aca7185ad8ad15130716be2cf4f6c5a4
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-02-09 16:04:01 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,19 @@ void main() {
   runApp(MyApp());
 }
 
+const Color kCanvasColor = Color(0xfff2f3f7);
+const String kGithubRepo = 'https://github.com/JideGuru/flutter_drawing_board';
+
 class MyApp extends StatelessWidget {
+// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: CustomPainterShapes(),
-      ),
-    );
+        title: 'Let\'s Draw',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: CustomPainterShapes(),
+        ));
   }
 }
