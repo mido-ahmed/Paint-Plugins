e5db2f19ee20b6bbc6a95f707d51ed8aa945839f
Mohamed Ahmed Hassan <96796474+mido-ahmed@users.noreply.github.com>
2023-02-10 15:25:02 +0200

    add another way to paint by Custom Painter with clear all button add source code in README.md

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A new Flutter project.
 
 ## Getting Started
-
+Source Code -> https://github.com/chooyan-eng/drawing_flutter/blob/main/lib/main.dart
 This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,6 +9,90 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Drawing Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _strokes = <Path>[];
+
+  void _startStroke(double x, double y) {
+    _strokes.add(Path()..moveTo(x, y));
+  }
+
+  void _moveStroke(double x, double y) {
+    setState(() {
+      _strokes.last.lineTo(x, y);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _strokes.clear();
+          });
+        },
+        child: Icon(Icons.undo),
+        backgroundColor: Colors.teal,
+      ),
+      appBar: AppBar(
+        title: Text('Drawing Flutter'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      body: GestureDetector(
+        onPanDown: (details) => _startStroke(
+          details.localPosition.dx,
+          details.localPosition.dy,
+        ),
+        onPanUpdate: (details) => _moveStroke(
+          details.localPosition.dx,
+          details.localPosition.dy,
+        ),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: CustomPaint(
+            painter: DrawingPainter(_strokes),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DrawingPainter extends CustomPainter {
+  final List<Path> strokes;
+
+  DrawingPainter(this.strokes);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (final stroke in strokes) {
+      final paint = Paint()
+        ..strokeWidth = 10
+        ..color = Colors.teal
+        ..style = PaintingStyle.stroke;
+
+      canvas.drawPath(stroke, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
